3febd7a5a92e5f7fb107db35f1e8c9324dfe685a
ksmo <ksmo@ksmo.ksmo>
2026-02-19 02:21:56 -0700

    remove junks

--- pages/serverless-check.js
+++
@@ -1,7 +0,0 @@
-export default function ServerlessCheck({ time }) {
-  return <div>Serverless page built at {time}</div>;
-}
-
-export async function getServerSideProps() {
-  return { props: { time: new Date().toISOString() } };
-}
