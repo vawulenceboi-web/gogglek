701124a5a45bed8a228961f43caca633768f9437
ksmo <ksmo@ksmo.ksmo>
2026-02-19 01:27:42 -0700

    fix serverless issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
 
-export default function App({component, pageProps }) {
-    return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
